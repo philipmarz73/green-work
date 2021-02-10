c627d72639bd6d9057177f084505e81fa52d7a22
Phlipper <marzullop73@gmail.com>
2021-02-10 17:01:10 -0500

    add Contact and About containers and routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./containers/About/About";
+import Contact from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1> 
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
